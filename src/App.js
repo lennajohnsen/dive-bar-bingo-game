0b4528a2aca660eafe91f0bffe9b05440008dad4
lennajohnsen <47152562+lennajohnsen@users.noreply.github.com>
2019-12-26 15:59:40 -0500

    editSign

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import BarSign from './components/BarSign';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="App">
       <header className="App-body">
+        <Intro/>
+
         <BarSign/>
 
         <p className="script">please remember to drink responsibly
-          <br></br> and support local businesses
+          <br></br> and support local businesses.
         </p>
-        
+
       </header>
     </div>
   );
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,24 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import beerClink from '../assets/favicon-16x16.png';
 
 export default class Intro extends Component {
 
     constructor(props) {
         super(props);
+        this.state = { visible: true } ;
         this.handleClose = this.handleClose.bind(this);
-        this.state = {
-            visible: true,
-        } ;
     }
 
-    handleClose(){
-        this.setState({ visbile: !this.state.false })
-        return alert('hallo')
+    handleClose = () => {
+        this.setState({ visible: !this.state.visible })
+        // console.log(this.state.visible)
     }
 
     render() {
         return (
-            <div onClick={this.handleClose} >
-                
+            <div  className={'modalDialog' + (this.state.visible ? "" : "invisible")} >
+                <div className="modal-content"> 
+                    <div className="modal-header">
+                            RULES AND REGULATIONS
+                    </div>
+
+                    <div className="modal-text">
+                    Lorem ipsum dolor amet plaid vegan cray organic, keffiyeh pug meditation. 
+                    Retro vice man braid bicycle rights vape wolf helvetica +1 crucifix sustainable flexitarian. 
+                    Meh letterpress pickled poke pour-over adaptogen unicorn literally 3 wolf moon kale chips kogi 
+                    stumptown heirloom bushwick. Cliche vegan trust fund, ramps street art banh mi cray raw denim 
+                    semiotics actually craft beer celiac. <br>
+                    
+                    </br><br>
+                    </br> Venmo portland keffiyeh keytar lumbersexual art party subway tile schlitz godard brunch tbh deep 
+                    v taiyaki. Organic migas live-edge keffiyeh kitsch hexagon. Single-origin coffee listicle meditation 
+                    shoreditch hoodie freegan. Glossier ethical pork belly post-ironic try-hard.
+                    </div>
+
+                    <div className="modal-footer" onClick = {this.handleClose} >
+                        <img src={beerClink} alt="beer" className="beer-icon"/>
+                        Yes, I am over 21
+                    </div>
+
+                </div>
             </div>
         )
     }
