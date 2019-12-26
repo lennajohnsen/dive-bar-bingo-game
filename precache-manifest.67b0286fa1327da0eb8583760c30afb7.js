f2b1af4ec53b723640123657d3686d0d1f8a5bd3
lennajohnsen <47152562+lennajohnsen@users.noreply.github.com>
2019-12-26 11:50:01 -0500

    Updates

---
+++ precache-manifest.67b0286fa1327da0eb8583760c30afb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68e7cecaed71884345870a7b3be3058",
+    "url": "/dive-bar-bingo-game/index.html"
+  },
+  {
+    "revision": "1ff06bf117e518dbf5bd",
+    "url": "/dive-bar-bingo-game/static/css/main.c83d34de.chunk.css"
+  },
+  {
+    "revision": "a64d53f7ac37beb66bf0",
+    "url": "/dive-bar-bingo-game/static/js/2.5e477da4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dive-bar-bingo-game/static/js/2.5e477da4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ff06bf117e518dbf5bd",
+    "url": "/dive-bar-bingo-game/static/js/main.0351bf51.chunk.js"
+  },
+  {
+    "revision": "8eb0a03f4d950fd214f1",
+    "url": "/dive-bar-bingo-game/static/js/runtime-main.4828891b.js"
+  },
+  {
+    "revision": "f5d147f8ff08f185f203f05538812b89",
+    "url": "/dive-bar-bingo-game/static/media/drawn-outline-01.f5d147f8.png"
+  },
+  {
+    "revision": "86b9c9270d2694ab28db9fb9f9df3399",
+    "url": "/dive-bar-bingo-game/static/media/sign-02.86b9c927.png"
+  }
+]);
